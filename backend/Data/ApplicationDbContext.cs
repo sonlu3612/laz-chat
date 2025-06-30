8cd3d48587216858e7f8e7beb7021189b9fc41ea
unknown <sonlu1207@gmail.com>
2025-07-01 06:19:37 +0700

    Initial commit

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
