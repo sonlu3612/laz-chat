7a60b150e7717a39cf9b77f4f033bb072d112365
hungtvlt <tuanhungvu0912@gmail.com>
2025-08-05 21:58:45 +0700

    InputGroup: modify style for elements

--- client/src/Components/InputGroup.jsx
+++ client/src/Components/InputGroup.jsx
@@ -13,17 +13,17 @@ export default function InputGroup(props) {
     <div className="px-4 pt-2 flex flex-col">
       <label className="">
         {title}
-        {isRequired && <span className="text-red-500"> *</span>}
+        {isRequired && <span className="text-light-error"> *</span>}
       </label>
 
       <input
-        className="border border-gray-300 rounded-xl p-2"
+        className="outline-2 outline-light-secondary focus:outline-4 focus:outline-light-primary rounded-xl p-2"
         placeholder={placeholder}
         type={type}
         value={value}
         onChange={onChange}
       ></input>
-      {errorMessage && <span className="text-red-500">{errorMessage}</span>}
+      {errorMessage && <span className="text-light-error">{errorMessage}</span>}
     </div>
   );
 }
