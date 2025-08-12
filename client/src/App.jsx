9e50725b0c14fc0aa4f438d62aeca3f34272ea3a
hungtvlt <tuanhungvu0912@gmail.com>
2025-08-12 15:46:34 +0700

    route: move to `App.jsx`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import './App.css';
-import AppRoutes from './routes';
-import { verifyToken } from './redux/reducers/auth';
+import React, { useEffect, Suspense } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import { verifyToken } from "./redux/reducers/auth";
+
+import ProtectedRoute from "./Components/auth/ProtectedRoute";
+
+const Home = React.lazy(() => import("./pages/Home"));
+const Login = React.lazy(() => import("./pages/Loginpage"));
+const Register = React.lazy(() => import("./pages/Register"));
+const Message = React.lazy(() => import("./pages/Message"));
 
 function App() {
+  const { user } = useSelector((state) => state.auth);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -12,9 +22,30 @@ function App() {
   }, [dispatch]);
 
   return (
-    <AppRoutes/>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/login"
+        element={
+          <ProtectedRoute user={!user} redirect="/">
+            <Login />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <ProtectedRoute user={!user} redirect="/">
+            <Register />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/message">
+        <Route index element={<Message />} />
+        <Route path=":id" element={<Message />} />
+      </Route>
+    </Routes>
+  );
 }
-
 
 export default App;
