ccc658891a41da4794ec935fe032bf326d6c2aad
hungtvlt <tuanhungvu0912@gmail.com>
2025-08-14 15:22:30 +0700

    add Overlay component

---
+++ client/src/Components/Overlay.jsx
@@ -0,0 +1,24 @@
+const Overlay = ({ children, isVisible }) => {
+  if (!isVisible) return null;
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        zIndex: 999,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Overlay;
