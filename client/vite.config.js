bca964794432ac8144cf7b4ae0c49f8fefb08afc
hungtvlt <tuanhungvu0912@gmail.com>
2025-09-23 22:08:31 +0700

    config: add for testing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  test: {
+    environment: "jsdom", // Add this - Use JSDOM as the test environment
+    setupFiles: ["./vitest-setup.js"],
+  },
+});
