c6e90bde8615f7eb5db73d419ba33e4534717b8f
SoL <sonlu1207@gmail.com>
2025-10-13 15:36:08 +0700

    Add: UpdateUserRequest

---
+++ server/Dtos/Users/UpdateUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace server.Dtos.Users
+{
+    public class UpdateUserRequest
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
